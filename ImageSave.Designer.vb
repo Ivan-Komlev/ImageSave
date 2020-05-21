6d205c4dfb6345e52ab033593132d1acce1cea59
JoomlaBoat <ivankomlev@gmail.com>
2020-05-21 15:37:00 -0500

    Initial commit

---
+++ ImageSave.Designer.vb
@@ -0,0 +1,213 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class ImageSave
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.PasteToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        Me.AutoSaveToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        Me.SaveAsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        Me.ToolStripMenuItem1 = New System.Windows.Forms.ToolStripSeparator
+        Me.SetFolderToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip
+        Me.MyStatus = New System.Windows.Forms.ToolStripStatusLabel
+        Me.cFolder = New System.Windows.Forms.ToolStripStatusLabel
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip
+        Me.ImageToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        Me.PasteToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem
+        Me.SaveToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        Me.SaveAsToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem
+        Me.ToolStripMenuItem2 = New System.Windows.Forms.ToolStripSeparator
+        Me.SetFolderToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.ContextMenuStrip1.SuspendLayout()
+        Me.StatusStrip1.SuspendLayout()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(0, 27)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(209, 238)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.PasteToolStripMenuItem, Me.AutoSaveToolStripMenuItem, Me.SaveAsToolStripMenuItem, Me.ToolStripMenuItem1, Me.SetFolderToolStripMenuItem})
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(179, 98)
+        '
+        'PasteToolStripMenuItem
+        '
+        Me.PasteToolStripMenuItem.Name = "PasteToolStripMenuItem"
+        Me.PasteToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Shift Or System.Windows.Forms.Keys.Insert), System.Windows.Forms.Keys)
+        Me.PasteToolStripMenuItem.Size = New System.Drawing.Size(178, 22)
+        Me.PasteToolStripMenuItem.Text = "Paste"
+        '
+        'AutoSaveToolStripMenuItem
+        '
+        Me.AutoSaveToolStripMenuItem.Enabled = False
+        Me.AutoSaveToolStripMenuItem.Name = "AutoSaveToolStripMenuItem"
+        Me.AutoSaveToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.S), System.Windows.Forms.Keys)
+        Me.AutoSaveToolStripMenuItem.Size = New System.Drawing.Size(178, 22)
+        Me.AutoSaveToolStripMenuItem.Text = "Auto Save"
+        '
+        'SaveAsToolStripMenuItem
+        '
+        Me.SaveAsToolStripMenuItem.Name = "SaveAsToolStripMenuItem"
+        Me.SaveAsToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.A), System.Windows.Forms.Keys)
+        Me.SaveAsToolStripMenuItem.Size = New System.Drawing.Size(178, 22)
+        Me.SaveAsToolStripMenuItem.Text = "Save As ..."
+        '
+        'ToolStripMenuItem1
+        '
+        Me.ToolStripMenuItem1.Name = "ToolStripMenuItem1"
+        Me.ToolStripMenuItem1.Size = New System.Drawing.Size(175, 6)
+        '
+        'SetFolderToolStripMenuItem
+        '
+        Me.SetFolderToolStripMenuItem.Enabled = False
+        Me.SetFolderToolStripMenuItem.Name = "SetFolderToolStripMenuItem"
+        Me.SetFolderToolStripMenuItem.Size = New System.Drawing.Size(178, 22)
+        Me.SetFolderToolStripMenuItem.Text = "Set Folder ..."
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.MyStatus, Me.cFolder})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 265)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(292, 22)
+        Me.StatusStrip1.TabIndex = 1
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'MyStatus
+        '
+        Me.MyStatus.Name = "MyStatus"
+        Me.MyStatus.Size = New System.Drawing.Size(38, 17)
+        Me.MyStatus.Text = "Ready"
+        '
+        'cFolder
+        '
+        Me.cFolder.Name = "cFolder"
+        Me.cFolder.Size = New System.Drawing.Size(15, 17)
+        Me.cFolder.Text = """"""
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ImageToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(292, 24)
+        Me.MenuStrip1.TabIndex = 2
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'ImageToolStripMenuItem
+        '
+        Me.ImageToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.PasteToolStripMenuItem1, Me.SaveToolStripMenuItem, Me.SaveAsToolStripMenuItem1, Me.ToolStripMenuItem2, Me.SetFolderToolStripMenuItem1})
+        Me.ImageToolStripMenuItem.Name = "ImageToolStripMenuItem"
+        Me.ImageToolStripMenuItem.Size = New System.Drawing.Size(49, 20)
+        Me.ImageToolStripMenuItem.Text = "Image"
+        '
+        'PasteToolStripMenuItem1
+        '
+        Me.PasteToolStripMenuItem1.Name = "PasteToolStripMenuItem1"
+        Me.PasteToolStripMenuItem1.ShortcutKeys = CType((System.Windows.Forms.Keys.Shift Or System.Windows.Forms.Keys.Insert), System.Windows.Forms.Keys)
+        Me.PasteToolStripMenuItem1.Size = New System.Drawing.Size(178, 22)
+        Me.PasteToolStripMenuItem1.Text = "Paste"
+        '
+        'SaveToolStripMenuItem
+        '
+        Me.SaveToolStripMenuItem.Enabled = False
+        Me.SaveToolStripMenuItem.Name = "SaveToolStripMenuItem"
+        Me.SaveToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.S), System.Windows.Forms.Keys)
+        Me.SaveToolStripMenuItem.Size = New System.Drawing.Size(178, 22)
+        Me.SaveToolStripMenuItem.Text = "Save"
+        '
+        'SaveAsToolStripMenuItem1
+        '
+        Me.SaveAsToolStripMenuItem1.Name = "SaveAsToolStripMenuItem1"
+        Me.SaveAsToolStripMenuItem1.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.A), System.Windows.Forms.Keys)
+        Me.SaveAsToolStripMenuItem1.Size = New System.Drawing.Size(178, 22)
+        Me.SaveAsToolStripMenuItem1.Text = "Save As ..."
+        '
+        'ToolStripMenuItem2
+        '
+        Me.ToolStripMenuItem2.Name = "ToolStripMenuItem2"
+        Me.ToolStripMenuItem2.Size = New System.Drawing.Size(175, 6)
+        '
+        'SetFolderToolStripMenuItem1
+        '
+        Me.SetFolderToolStripMenuItem1.Enabled = False
+        Me.SetFolderToolStripMenuItem1.Name = "SetFolderToolStripMenuItem1"
+        Me.SetFolderToolStripMenuItem1.Size = New System.Drawing.Size(178, 22)
+        Me.SetFolderToolStripMenuItem1.Text = "Set Folder ..."
+        '
+        'ImageSave
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoScroll = True
+        Me.ClientSize = New System.Drawing.Size(309, 266)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "ImageSave"
+        Me.Text = "Image Paster"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ContextMenuStrip1.ResumeLayout(False)
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents ContextMenuStrip1 As System.Windows.Forms.ContextMenuStrip
+    Friend WithEvents PasteToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents AutoSaveToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents SaveAsToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem1 As System.Windows.Forms.ToolStripSeparator
+    Friend WithEvents SetFolderToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents SaveFileDialog1 As System.Windows.Forms.SaveFileDialog
+    Friend WithEvents FolderBrowserDialog1 As System.Windows.Forms.FolderBrowserDialog
+    Friend WithEvents StatusStrip1 As System.Windows.Forms.StatusStrip
+    Friend WithEvents MyStatus As System.Windows.Forms.ToolStripStatusLabel
+    Friend WithEvents cFolder As System.Windows.Forms.ToolStripStatusLabel
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents ImageToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents PasteToolStripMenuItem1 As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents SaveToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents SaveAsToolStripMenuItem1 As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem2 As System.Windows.Forms.ToolStripSeparator
+    Friend WithEvents SetFolderToolStripMenuItem1 As System.Windows.Forms.ToolStripMenuItem
+
+End Class
---
+++ ImageSave.vb
@@ -0,0 +1,89 @@
+﻿Public Class ImageSave
+    Private CurrentFolder As String = ""
+
+    Private Sub PasteToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles PasteToolStripMenuItem.Click
+
+        Try
+            Me.PictureBox1.Image = My.Computer.Clipboard.GetImage
+        Catch ex As Exception
+            Exit Sub
+        End Try
+
+
+
+    End Sub
+
+    Private Sub PictureBox1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles PictureBox1.Click
+
+    End Sub
+
+    Private Sub PictureBox1_MouseUp(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles PictureBox1.MouseUp
+        If e.Button = Windows.Forms.MouseButtons.Right Then
+
+            ContextMenuStrip1.Show(Control.MousePosition.X, Control.MousePosition.Y)
+        End If
+    End Sub
+
+    Private Sub Form1_MouseUp(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles Me.MouseUp
+        If e.Button = Windows.Forms.MouseButtons.Right Then
+
+            ContextMenuStrip1.Show(Control.MousePosition.X, Control.MousePosition.Y)
+        End If
+    End Sub
+
+    Private Sub SetFolder()
+        FolderBrowserDialog1.ShowNewFolderButton = True
+
+
+        If FolderBrowserDialog1.ShowDialog() = Windows.Forms.DialogResult.OK Then
+            CurrentFolder = FolderBrowserDialog1.SelectedPath & "\"
+
+
+            cFolder.Text = CurrentFolder
+        End If
+    End Sub
+
+    Private Sub SetFolderToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles SetFolderToolStripMenuItem.Click
+        SetFolder()
+    End Sub
+
+    
+    Private Sub SaveAsToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles SaveAsToolStripMenuItem.Click
+        SaveAsImage()
+
+    End Sub
+    Private Sub SaveAsImage()
+        SaveFileDialog1.AddExtension = False
+        SaveFileDialog1.CheckFileExists = False
+        SaveFileDialog1.CheckPathExists = True
+        SaveFileDialog1.FileName = ""
+        SaveFileDialog1.InitialDirectory = My.Computer.FileSystem.CurrentDirectory
+        SaveFileDialog1.Filter = "Images (jpg)|*.jpg;"
+        SaveFileDialog1.AddExtension = True
+
+        If SaveFileDialog1.ShowDialog() = Windows.Forms.DialogResult.OK Then
+            Me.PictureBox1.Image.Save(SaveFileDialog1.FileName, Imaging.ImageFormat.Jpeg)
+        End If
+    End Sub
+
+    Private Sub PasteToolStripMenuItem1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles PasteToolStripMenuItem1.Click
+
+        Try
+            Me.PictureBox1.Image = My.Computer.Clipboard.GetImage
+        Catch ex As Exception
+            Exit Sub
+        End Try
+
+
+
+    End Sub
+
+  
+    Private Sub SaveAsToolStripMenuItem1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles SaveAsToolStripMenuItem1.Click
+        SaveAsImage()
+    End Sub
+
+    Private Sub SetFolderToolStripMenuItem1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles SetFolderToolStripMenuItem1.Click
+        SetFolder()
+    End Sub
+End Class
